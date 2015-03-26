166fce6074f1278d4d67fef25577d8e8f8c92c0e
jasvindersingh-evon <jasvinder.singh@evontech.com>
2015-03-26 16:07:09 +0530

    Initial Committ

--- SwiftProgramming/FirstViewController.swift
+++ SwiftProgramming/FirstViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var lbl1: UILabel!
+    @IBOutlet weak var lbl2: UILabel!
+    var value1 = 0
+    var value2 = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lbl1.text = "\(value1)"
+        lbl2.text = "\(value2)"
     }
 
     override func didReceiveMemoryWarning() {
---
+++ SwiftProgramming/LoginViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  SwiftProgramming
+//
+//  Created by jassi on 26/03/15.
+//  Copyright (c) 2015 jassi. All rights reserved.
+//
+
+import UIKit
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+
+        //Method 1
+//        var tabBarVC:UITabBarController? = segue.destinationViewController as? UITabBarController
+//        var viewControllers = tabBarVC?.viewControllers as NSArray!
+//        var VC2: FirstViewController = viewControllers.objectAtIndex(0) as FirstViewController
+//        VC2.value1 = 30;
+//        VC2.value2 = 40;
+        
+        //Method 2
+        var tabVC: AnyObject = segue.destinationViewController
+        var vCArr:Array = tabVC.customizableViewControllers!!
+        var first:FirstViewController = vCArr[0] as FirstViewController
+        first.value1 = 30;
+        first.value2 = 40;
+
+
+    }
+    
+
+}
