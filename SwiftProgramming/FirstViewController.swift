a073995ef8d747a246435d74ea49e714faeb4abb
jasvindersingh-evon <jasvinder.singh@evontech.com>
2015-03-26 13:31:07 +0530

    Initial Commit

---
+++ SwiftProgramming/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftProgramming
+//
+//  Created by jassi on 26/03/15.
+//  Copyright (c) 2015 jassi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
